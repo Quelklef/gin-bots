650d31cc44008d933846886ca9746e375328eb81
samm81 <samwmaynard@gmail.com>
2019-12-29 01:26:40 -0500

    some hands for debugging purposes

---
+++ debug_hands.py
@@ -0,0 +1,40 @@
+from cards import Card
+
+test_hand1 = {
+  Card('H7'),
+  Card('C2'),
+  Card('S4'),
+  Card('H2'),
+  Card('H1'),
+  Card('S2'),
+  Card('C7'),
+  Card('H3'),
+  Card('H9'),
+  Card('D7'),
+}
+
+test_hand2 = {
+  Card('S1'),
+  Card('S2'),
+  Card('S3'),
+  Card('D4'),
+  Card('D5'),
+  Card('H7'),
+  Card('S7'),
+  Card('C9'),
+  Card('D9'),
+  Card('H9'),
+}
+
+test_hand3 = {
+  Card('SA'),
+  Card('C2'),
+  Card('C3'),
+  Card('C4'),
+  Card('S4'),
+  Card('H6'),
+  Card('S8'),
+  Card('C8'),
+  Card('C9'),
+  Card('S9'),
+}
