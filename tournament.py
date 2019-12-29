a0acaee5b678baec00e9e8a58da6c8097b0ffd8e
Eli <elimaynard923@gmail.com>
2019-12-28 21:44:19 -0800

    have tournaments play multiple hands per pair

--- tournament.py
+++ tournament.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 from pathlib import Path
 import itertools as it
-from collections import namedtuple
+import statistics as stat
 
 import communication
 import gin
@@ -26,27 +26,38 @@ class GinBot:
     return result
 
 
-def do_tournament(bots):
+def do_tournament(bots, n=20):
   """ For a list of bots, pit each bot against every other """
 
   # Pit every bot against each other
   matches = list(it.combinations(bots, 2))
-  scores = { match: 0 for match in matches }
+  scores = { match: [] for match in matches }
 
   for match in matches:
     bot1, bot2 = match
+    print(f"\n#= {bot1} vs {bot2} =#:")
 
-    print(f"{bot1} vs {bot2}... ", end='', flush=True)
+    for i in range(n):
+      print(f"Hand #{i + 1}/{n}... ", end='', flush=True)
 
-    result = gin.play_hand(bot1, bot2)
-    scores[match] = result
+      result = gin.play_hand(bot1, bot2)
+      scores[match].append(result)
 
-    if result == 0:
-      print("tie")
-    else:
-      winner = bot1 if result > 0 else bot2
-      winner_score = abs(result)
-      print(f"{winner} wins for {winner_score} points")
+      if result == 0:
+        print("tie")
+      else:
+        winner = bot1 if result > 0 else bot2
+        winner_score = abs(result)
+        print(f"{winner} wins for {winner_score} points")
+
+  print("\n\n\n#== Scoreboard ==#")
+
+  for match in matches:
+    bot1, bot2 = match
+    games = scores[match]
+    mean = stat.mean(games)
+    stdev = stat.stdev(games)
+    print(f"{bot1} vs {bot2}: mean = {mean:+.2f}, stdev = {stdev:.2f}")
 
 
 def main():
